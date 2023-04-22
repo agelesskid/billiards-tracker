c918eaede59e6d6e461024ccfb21e1aa1559b56b
ageless <agelesskidxd@gmail.com>
2023-04-22 12:18:51 +0200

    changed Table html, styles

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,23 +6,25 @@ export default function Table(){
             <div className={styles.table}>
                 <h2>Table 1</h2>
                 <div className={styles["table-content-wrapper"]}>
-                <div className={styles["table-users"]}>
-                    <input type="text" name="user-name" id="user-name-input" placeholder="Name" />
-                    <div className={styles["user-details-wrapper"]}>
-                    <div className={styles["user-time-wrapper"]}>
-                        <input type="number" name="user-time" id="user-time-input" placeholder="time"/>
-                        <p>min.</p>
+                    <div className={styles["table-users"]}>
+                        <div className={styles["table-user"]}>
+                            <input type="text" name="user-name" id="user-name-input" placeholder="Name" />
+                            <div className={styles["user-details-wrapper"]}>
+                                <div className={styles["user-time-wrapper"]}>
+                                    <input type="number" name="user-time" id="user-time-input" placeholder="time"/>
+                                    <p>min.</p>
+                                </div>
+                                <span className={styles["user-cost"]}>0€</span>
+                            </div>
+                        </div>
                     </div>
-                    <p className={styles["user-cost"]}>cost</p>
+                    <div className={styles["table-controls"]}>
+                        <div className={styles["table-price-wrapper"]}>
+                            <input className={styles["table-price-input"]} type="number" name="table-price" id="table-price-input"/>
+                            <p className={styles["table-price-text"]}>€/hour</p>
+                        </div>
+                        <button type="button">+ add user</button>
                     </div>
-                </div>
-                <div className={styles["table-controls"]}>
-                    <div className={styles["table-price-wrapper"]}>
-                    <input type="number" name="table-price" id="table-price-input"/>
-                    <p className={styles["table-price-text"]}>€/hour</p>
-                    <button type="button">+ add user</button>
-                    </div>
-                </div>
                 </div>
             </div>
         </>
